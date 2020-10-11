95d5307f1198f434571b79cc2a30b8421661fb5a
Ali Syihab <ali.syihab@gmail.com>
2020-10-11 15:35:44 +0700

    installation

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <Container>
+      <Button variant="primary">hello</Button>
+    </Container>
+  );
+}
+
+export default App;
